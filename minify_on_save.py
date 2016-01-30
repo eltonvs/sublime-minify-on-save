093d1dbab4312d76fdeae579256aa43c316f9b7c
Elton Viana <eltonvs@outlook.com>
2016-01-30 08:13:47 -0300

    Fix comparison

--- minify_on_save.py
+++ minify_on_save.py
@@ -5,5 +5,5 @@ class MinifyOnSave(sublime_plugin.EventListener):
         file_types_to_minify = ['js', 'css']
         not_minify = ['min']
         file_name_parts = view.file_name().split('.')
-        if file_name_parts[-1:] in file_types_to_minify and file_name_parts[-2:-1] not in not_minify:
+        if file_name_parts[-1] in file_types_to_minify and file_name_parts[-2] not in not_minify:
             view.run_command('minify_to_file')
